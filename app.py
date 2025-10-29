4f9d2d8dfe9352aea745810577c7c2b81c4a2111
Jie-Yan094 <jarita0904@gmail.com>
2025-10-29 02:29:04 +0000

    new file:   app.py new file:   page_3dmap-2.py new file:   page_home.py

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+# 1. 使用 st.Page() 定義所有頁面
+# 注意：st.Page() 會自動尋找 .py 檔案
+pages = [
+    st.Page("page_home.py", title="專案首頁", icon="🏠"),
+    st.Page("page_3dmap-2.py", title="Plotly 3D互動地圖瀏覽", icon="🗺️")
+]
+
+# 2. 使用 st.navigation() 建立導覽 (例如在側邊欄)
+with st.sidebar:
+    st.title("關於我：自我介紹")
+    # st.navigation() 會回傳被選擇的頁面
+    selected_page = st.navigation(pages)
+
+
+# 3. 執行被選擇的頁面
+selected_page.run()
